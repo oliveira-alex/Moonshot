897e86347018b6cc8c8b7643015c5a7ab31c5811
ALeX <alexdeoliveira.poli@gmail.com>
2021-05-10 11:15:00 -0300

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Alex Oliveira on 10/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
