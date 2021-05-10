19780452713da85bbf27a1bc7b246d9556750cd3
ALeX <alexdeoliveira.poli@gmail.com>
2021-05-10 20:55:01 -0300

    Moonshot SwiftUI Tutorial 7 of 10 - Formatting our mission view

--- Moonshot/ContentView.swift
+++ Moonshot/ContentView.swift
@@ -12,8 +12,23 @@ struct ContentView: View {
     let missions: [Mission] = Bundle.main.decode("missions.json")
     
     var body: some View {
-        Text("\(missions.count)")
-            .padding()
+        NavigationView {
+            List(missions) { mission in
+                NavigationLink(destination: Text("Detail View")) {
+                    Image(mission.image)
+                        .resizable()
+                        .scaledToFit() //same as .aspectRatio(contentMode: .fit)
+                        .frame(width: 44, height: 44)
+                    
+                    VStack(alignment: .leading) {
+                        Text(mission.displayName)
+                            .font(.headline)
+                        Text(mission.formattedLaunchDate)
+                    }
+                }
+            }
+            .navigationBarTitle("Moonshot")
+        }
     }
 }
 
